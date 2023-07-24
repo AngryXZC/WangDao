52bee6def495102038dbdb93184fc54a98f0fc0d
AngryLeesin <xingzhicheng@imudges.com>
2023-07-24 18:19:12 +0800

    解释一下需要移动的次数

--- src/linerList/staticSequenceList.cpp
+++ src/linerList/staticSequenceList.cpp
@@ -13,6 +13,9 @@ bool ListInsert(SqList &L, int i, int e)
     // Judge whether current storage space is full or not
     if (L.length >= MAXSIZE)
         return false;
+    // 逻辑结构上位序从一开始，而数组从0开始
+    // 在逻辑结构的第i个位置插入元素，需要进行（l.length-i）+1次移动
+    // 所以这里有等于号
     for (int j = L.length; j >= i; j--)
     {
         L.data[j] = L.data[j - 1];
